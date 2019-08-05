141c3c48434d7ea658e9280f35aa66004322a5a6
bit-user <taehoon@naver.com>
2019-08-05 19:47:17 +0900

    init commit

--- src/helloworld/helloworld.java
+++ src/helloworld/helloworld.java
@@ -4,6 +4,13 @@ public class helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("qweqwe");
+		System.out.println("qweqwe");
+		System.out.println("qweqwe");
+		System.out.println("qweqwe");
+		System.out.println("qweqwe");
+		System.out.println("qweqwe");
+		
+		
 	}
 
 }
